e861c25a8c2af423f17905eea4429df9fe995f28
Rich Layte <rich.layte@gmail.com>
2013-07-01 17:18:11 +0100

    render all children when rendered

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 window.$ = require('./vendor/jquery.js');
 var Backbone = require('backbone');
+var _ = require('underscore');
 
 module.exports = Backbone.View.extend({
 
+  constructor: function() {
+    this._children = [];
+
+    Backbone.View.apply(this, arguments);
+  },
+
   add: function(child) {
+    this._children.push(child);
     this.$el.append(child.$el);
+  },
+
+  render: function() {
+    _.each(this._children, function(child) {
+      child.render();
+    });
   }
 });
--- spec/view_spec.js
+++ spec/view_spec.js
@@ -4,14 +4,35 @@ describe("View", function() {
   
   describe("adding a child view", function() {
     
-    it("should render the child element", function() {
+    it("should add the child element", function() {
       var view1 = new View;
       var view2 = new View({ id: 'view2' });
 
       view1.add(view2);
       view1.render();
 
+      spyOn(view2, 'render');
+
       expect(view1.$el.find('#view2').length).toBe(1);
+    });
+  });
+
+  describe("rendering", function() {
+    
+    it("should render all its children", function() {
+      var view1 = new View;
+      var view2 = new View({ id: 'view2' });
+      var view3 = new View({ id: 'view3' });
+
+      spyOn(view2, 'render');
+      spyOn(view3, 'render');
+
+      view1.add(view2);
+      view1.add(view3);
+      view1.render();
+
+      expect(view2.render).toHaveBeenCalled();
+      expect(view3.render).toHaveBeenCalled();
     });
   });
 });
