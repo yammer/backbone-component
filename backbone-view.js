62090db1e0b59452b86a3531587fc8e755fccd14
Rich Layte <rich.layte@gmail.com>
2013-07-01 16:38:27 +0100

    add backbone-view (built js file

---
+++ backbone-view.js
@@ -0,0 +1,4 @@
+;(function(e,t,n,r){function i(r){if(!n[r]){if(!t[r]){if(e)return e(r);throw new Error("Cannot find module '"+r+"'")}var s=n[r]={exports:{}};t[r][0](function(e){var n=t[r][1][e];return i(n?n:e)},s,s.exports)}return n[r].exports}for(var s=0;s<r.length;s++)i(r[s]);return i})(typeof require!=="undefined"&&require,{1:[function(require,module,exports){
+
+},{}]},{},[1])
+;
