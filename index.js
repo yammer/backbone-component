4d4f30b0b758ac1b8b63fa7ac870f19c65d1948c
Rich Layte <rich.layte@gmail.com>
2013-07-01 17:04:12 +0100

    add child view to parent

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ window.$ = require('./vendor/jquery.js');
 var Backbone = require('backbone');
 
 module.exports = Backbone.View.extend({
+
+  add: function(child) {
+    this.$el.append(child.$el);
+  }
 });
