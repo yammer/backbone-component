22d532665cec35f362669f40d0f4baf33945322b
Rich Layte <rich.layte@gmail.com>
2013-07-03 23:10:36 +0100

    add documentation comments

--- backbone-view.js
+++ backbone-view.js
@@ -1,5 +1,13 @@
+// Backbone.Component
+// ==================
+// 
+// A thin layer on top of Backbone's view class to add nested child views.
+//
+// ---
+
 Backbone.Component = Backbone.View.extend({
 
+  // Override constructor so Components can use initialise as normal.
   constructor: function() {
     this._children = [];
     this.add = this.append;
@@ -8,16 +16,25 @@ Backbone.Component = Backbone.View.extend({
     Backbone.View.apply(this, arguments);
   },
 
+  // Public API
+  // ----------
+  // ----------
+
+  // Add a child view to the end of the element.
+  // Optionally specify a selector within the view to attach to.
   append: function(view, selector) {
     this._addChild(view, selector, 'append');
     return this;
   },
 
+  // Add a child view to the beginning of the view's element.
+  // Optionally specify a selector within the view to attach to.
   prepend: function(view, selector) {
     this._addChild(view, selector, 'prepend');
     return this;
   },
 
+  // Remove itself and all children from the DOM
   remove: function() {
     this._removeFromParent();
     this._removeChildren();
@@ -25,15 +42,24 @@ Backbone.Component = Backbone.View.extend({
     return this;
   },
 
+  // Private methods
+  // ----------
+  // ----------
+
+  // Add a child view to an internal array keeping a reference to
+  // element and attach method it should use.
   _addChild: function(view, selector, method) {
     var child = { view: view, selector: selector, method: method };
 
+    // Assign a method to the child so it can remove itself from
+    // `_children` array when it's removed.
     var removeFromParent = _.bind(this._removeFromParent, this);
     view._removeFromParent = _.partial(removeFromParent, child);
 
     this._children.push(child);
   },
 
+  // Call remove for each child added to the view.
   _removeChildren: function() {
     _.invoke(_.pluck(this._children, 'view'), 'remove');
   },
@@ -42,6 +68,7 @@ Backbone.Component = Backbone.View.extend({
     this._children = _.without(this._children, child);
   },
 
+  // Wrap render to automatically attach all children.
   _wrapRender: function() {
     var wrapper = function(render) {
       var args = Array.prototype.slice.call(arguments, 1);
@@ -54,6 +81,8 @@ Backbone.Component = Backbone.View.extend({
     this.render = _.wrap(originalRender, wrapper);
   },
 
+  // Attach child to the correct element and with correct method.
+  // Defaults to `this.$el` and `append`.
   _attachChild: function(child) {
     var method = child.method || 'append';
     var target = child.selector ? this.$(child.selector) : this.$el;
@@ -62,6 +91,9 @@ Backbone.Component = Backbone.View.extend({
     target[method](child.view.$el);
   },
 
+  // Attach all children in the right order.
+  // Call `delegateEvents` for each child view so handlers are correctly
+  // bound after being attached.
   _attachChildren: function() {
     _.each(this._children, function(child) {
       this._attachChild(child);
