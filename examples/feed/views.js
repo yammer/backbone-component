f51e479837d7b3093a08231d8efcd9add6a04f13
Rich Layte <rich.layte@gmail.com>
2013-07-03 18:01:31 +0100

    separate views and models into different files in feed example

--- examples/feed/views.js
+++ examples/feed/views.js
@@ -1,28 +1,3 @@
-var Message = Backbone.Model.extend({
-  validate: function() {
-    return this.get('body').length > 0;
-  }
-});
-
-var Messages = Backbone.Collection.extend({
-  model: Message
-});
-
-var Thread = Backbone.Model.extend({
-
-  initialize: function() {
-    this.messages = new Messages;
-  },
-
-  addMessage: function(message) {
-    this.messages.add(message);
-  }
-});
-
-var Threads = Backbone.Collection.extend({
-  model: Thread
-});
-
 var Publisher = View.extend({
 
   className: 'publisher',
