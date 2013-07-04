ca088b689c2803e3c4383bd0ba106eb711218aea
Rich Layte <rich.layte@gmail.com>
2013-07-04 12:35:37 +0100

    move method/target logic to addChild

--- backbone-component.js
+++ backbone-component.js
@@ -44,7 +44,8 @@ Backbone.Component = Backbone.View.extend({
   // Add a child view to an internal array, keeping a reference to
   // the element and attach method it should use.
   _addChild: function(view, selector, method) {
-    var child = { view: view, selector: selector, method: method };
+    var target = selector ? this.$(selector) : this.$el;
+    var child = { view: view, target: target, method: method || 'append' };
 
     // Assign a method to the child so it can remove itself from
     // `_children` array when it's removed.
@@ -93,10 +94,7 @@ Backbone.Component = Backbone.View.extend({
   // Attach child to the correct element and with correct method.
   // Defaults to `this.$el` and `append`.
   _attachChild: function(child) {
-    var method = child.method || 'append';
-    var target = child.selector ? this.$(child.selector) : this.$el;
-
-    target[method](child.view.render().$el);
+    child.target[child.method](child.view.render().$el);
   },
 
   // Attach all children in the right order.
